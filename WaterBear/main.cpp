18ba6b693d637ef2a3d1c3e7fb966087025fd42e
Matt Bradberry <mcbradberry@gmail.com>
2016-03-27 15:15:14 -0700

    spinny cube demo

--- WaterBear/main.cpp
+++ WaterBear/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include "ext/math/vmath.h"
 
 class RenderingApp : public WaterBear::Core::Application {
 public:
@@ -57,10 +58,10 @@ public:
         mProgram = glCreateProgram();
         
         GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-        CompileShader(fs, "single_tri_fs.glsl");
+        CompileShader(fs, "spinny_cube_fs.glsl");
         
         GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-        CompileShader(vs, "single_tri_vs.glsl");
+        CompileShader(vs, "spinny_cube_vs.glsl");
         
         glAttachShader(mProgram, fs);
         glAttachShader(mProgram, vs);
@@ -69,16 +70,82 @@ public:
         glDeleteShader(fs);
         glDeleteShader(vs);
         
+        mModelViewLoc = glGetUniformLocation(mProgram, "mv_matrix");
+        mProjLoc = glGetUniformLocation(mProgram, "proj_matrix");
+        
         glGenVertexArrays(1, &mVao);
         glBindVertexArray(mVao);
+        
+        static const GLushort vertIndices[] =
+        {
+            0, 1, 2,
+            2, 1, 3,
+            2, 3, 4,
+            4, 3, 5,
+            4, 5, 6,
+            6, 5, 7,
+            6, 7, 0,
+            0, 7, 1,
+            6, 0, 2,
+            2, 4, 6,
+            7, 5, 3,
+            7, 3, 1
+        };
+        
+        static const GLfloat vertPositions[] =
+        {
+            -0.25f, -0.25f, -0.25f,
+            -0.25f,  0.25f, -0.25f,
+            0.25f, -0.25f, -0.25f,
+            0.25f,  0.25f, -0.25f,
+            0.25f, -0.25f,  0.25f,
+            0.25f,  0.25f,  0.25f,
+            -0.25f, -0.25f,  0.25f,
+            -0.25f,  0.25f,  0.25f,
+        };
+        
+        glGenBuffers (1, &mPositionBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, mPositionBuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertPositions), vertPositions, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+        glEnableVertexAttribArray(0);
+        
+        glGenBuffers(1, &mIndexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIndexBuffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertIndices), vertIndices, GL_STATIC_DRAW);
+        
+        glEnable(GL_CULL_FACE);
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LEQUAL);
     }
     
     void OnRender(double currentTime) {
         static const GLfloat green[] = { 0.0f, 0.25f, 0.0f, 1.0f };
+        static const float one = 1.0f;
+        
+        glViewport(0, 0, config.windowWidth, config.windowHeight);
         glClearBufferfv(GL_COLOR, 0, green);
+        glClearBufferfv(GL_DEPTH, 0, &one);
         
         glUseProgram(mProgram);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        // projection matrix
+        vmath::mat4 projMatrix = vmath::perspective(50.0f, (float)config.windowWidth / (float)config.windowHeight, 0.1f, 1000.0f);
+        glUniformMatrix4fv(mProjLoc, 1, GL_FALSE, projMatrix);
+        
+        // model-view matrix
+        float f = (float)currentTime * 0.3f;
+        vmath::mat4 mvMatrix = vmath::translate(0.0f, 0.0f, -4.0f) *
+        vmath::translate(sinf(2.1f * f) * 0.5f,
+                         cosf(1.7f * f) * 0.5f,
+                         sinf(1.3f * f) * cosf(1.5f * f) * 2.0f) *
+        vmath::rotate((float)currentTime * 45.0f, 0.0f, 1.0f, 0.0f) *
+        vmath::rotate((float)currentTime * 81.0f, 1.0f, 0.0f, 0.0f);
+        
+        glUniformMatrix4fv(mModelViewLoc, 1, GL_FALSE, mvMatrix);
+        
+        // draw
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
     }
     
     void OnShutdown() {
@@ -86,6 +153,9 @@ public:
         
         glDeleteVertexArrays(1, &mVao);
         glDeleteProgram(mProgram);
+        
+        glDeleteBuffers(1, &mPositionBuffer);
+        glDeleteBuffers(1, &mIndexBuffer);
     }
     
     void OnWindowResize(int w, int h) {
@@ -95,13 +165,10 @@ public:
 private:
     GLuint mProgram;
     GLuint mVao;
+    GLuint mModelViewLoc;
+    GLuint mProjLoc;
+    GLuint mPositionBuffer;
+    GLuint mIndexBuffer;
 };
 
-//DECLARE_MAIN(RenderingApp)
-int main(int argc, const char** argv) {
-    RenderingApp *app = new RenderingApp();
-    app->Run(app);
-    delete app;
-    return 0;
-}
-
+DECLARE_MAIN(RenderingApp)
